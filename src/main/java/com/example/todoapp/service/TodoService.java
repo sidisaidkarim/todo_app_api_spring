4146cb00a8d24d1f135de38e9f1fa9cc26647d16
sskarim85@gmail.com <sskarim85@gmail.com>
2023-11-29 09:26:14 +0100

    added service, repo, resource folder + get all todo

---
+++ src/main/java/com/example/todoapp/service/TodoService.java
@@ -0,0 +1,21 @@
+package com.example.todoapp.service;
+
+import com.example.todoapp.model.Todo;
+import com.example.todoapp.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getAll(){
+        return this.todoRepository.findAll();
+    }
+}
